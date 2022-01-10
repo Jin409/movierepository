4e5a9eb08c7de910521a8155e5344cff12ceb544
seunghee jin <sualng123@gmail.com>
2022-01-11 00:31:55 +0900

    (feat)내브바로그인로그아웃 회원 구분

--- movie/main/views.py
+++ movie/main/views.py
@@ -5,13 +5,32 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     user_id = request.session.get('user')
+
     info_msg = {}
+
     if user_id:
         user = User.objects.get(pk = user_id)
         info_msg['welcome'] = "%s님 환영합니다!"%user.user_nickname
-       
-    return render(request,'home.html',info_msg,user_id)
+        
+
+    else:
+        info_msg['please'] = "로그인을 해주세요!"
+    return render(request,'home.html',info_msg)
+
+
         
 
 def detail(request):
-    return render(request,'detail.html')
+    user_id = request.session.get('user')
+    info_msg = {}
+
+    if user_id:
+        user = User.objects.get(pk = user_id)
+        info_msg['welcome'] = "%s님 환영합니다!"%user.user_nickname
+        
+
+    else:
+        info_msg['please'] = "로그인을 해주세요!"
+    
+
+    return render(request,'detail.html',info_msg)
